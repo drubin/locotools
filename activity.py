88546d39d7608c4e5b1ad5b010624b28a3cdf907
Paul Tagliamonte <paultag@ubuntu.com>
2010-05-25 13:32:44 -0400

    does karma now ( THANKS popey! )

--- activity.py
+++ activity.py
@@ -16,6 +16,7 @@ output    = "./data/dataset"
 ############################################################
 
 from launchpadlib.launchpad import Launchpad
+from launchpadlib.errors import HTTPError
 
 import time
 import datetime
@@ -48,15 +49,46 @@ print "Fetching Details"
 
 members = team.members_details
 
-d_mini = 0 # data min
-d_maxi = 0 # data max
+d_mini = 0  # data min
+d_maxi = 0  # data max
+k_mini = -1 # karma min
+k_maxi = 0  # karma max
+
+rolling_karma = -1
 
 print "Starting to Process..."
 
+count = 0
 for member in members:
-    d = member.date_joined
-    membership[member.member.name] = time.mktime(d.timetuple())
-    print "  Processed", member.member.name
+    try:
+        d = member.date_joined
+        posix_join_date = time.mktime(d.timetuple())
+        membership[member.member.name] = { "join" : posix_join_date, "karma" : member.member.karma }
+
+	karma = member.member.karma
+
+        if k_mini > karma or k_mini == -1:
+            k_mini = karma
+        if k_maxi < karma:
+            k_maxi = karma
+
+        if rolling_karma == -1:
+            rolling_karma = karma
+        else:
+            rolling_karma = ( rolling_karma + karma ) / 2
+
+        if d_mini > posix_join_date or d_mini == 0:
+            d_mini = posix_join_date
+        if d_maxi < posix_join_date:
+            d_maxi = posix_join_date
+
+        print "  Processed " + member.member.name + "  ( %04d )" % count
+        count = count +1 
+    except HTTPError, e:
+	print "HTTP Error"
+
+print "Start date: " + str(d_mini) + ", end date " + str(d_maxi)
+print "Karma avg: " + str(rolling_karma) + ", max: " + str(k_maxi) + ", min: " + str(k_mini)
 
 print "Writitng Data to ", output
 f = open(output, 'w')
